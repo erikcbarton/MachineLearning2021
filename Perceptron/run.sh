c679b9489574ecff1c54858d889d29d4432e54b0
Erik Barton <erikcbarton@gmail.com>
2021-10-31 15:48:47 -0600

    Adjusted printing.

--- Perceptron/run.sh
+++ Perceptron/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Use comments to reduce the number of portions of the assignment run
 python3 Perceptron.py a
-#python3 Perceptron.py b
-#python3 Perceptron.py c
+python3 Perceptron.py b
+python3 Perceptron.py c
