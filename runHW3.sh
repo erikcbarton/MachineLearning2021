27e4734426ad0d63e139f0e949ae9cedd1185a7b
Erik Barton <erikcbarton@gmail.com>
2021-11-01 08:58:05 -0600

    Updates to readme and added additional run.sh.

---
+++ runHW3.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Use comments to reduce the number of portions of the assignment run
+python3 ./Perceptron/Perceptron.py a
+python3 ./Perceptron/Perceptron.py b
+python3 ./Perceptron/Perceptron.py c
